e1dde3530b1190f0d2bd31c1a1622c6c35daecf9
ksyydream <249499109@qq.com>
2016-12-19 15:49:07 +0800

    llx

--- Baocms/Lib/Action/Apipublic/CommonAction.class.php
+++ Baocms/Lib/Action/Apipublic/CommonAction.class.php
@@ -65,4 +65,10 @@ class CommonAction extends Action{
         }
         return $data['face'];
     }
+    public function share(){
+        //include '../phpqrcode.php';
+        //http://$Think.SERVER.HTTP_HOST}>__ROOT__<{:U('/mobile/shop/detail',array('shop_id'=>$shop_id))}>?uid=<{$uid}>
+        $shop_id=$this->_param['shop_id'];
+        QRcode::png(getSiteUrl().U('/Apiuser/Team/index',array('shop_id'=>$shop_id))."?uid=".$this->app_uid);
+    }
 }
