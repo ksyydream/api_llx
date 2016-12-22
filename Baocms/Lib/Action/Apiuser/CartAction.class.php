deaa38845856ecf995e41c0e901ce841e598fafc
ksyydream <249499109@qq.com>
2016-12-22 14:28:10 +0800

    llx

---
+++ Baocms/Lib/Action/Apiuser/CartAction.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/12/20
+ * Time: 11:03
+ */
+class CartAction extends CommonAction {
+
+    public function cartadd()
+    {
+            $goods_id = (int) $this->_post('goods_id');
+            if (empty($goods_id)) {
+                $rs = array('success' => false, 'error_msg'=>'请选择商品!');
+                die(json_encode($rs));
+            }
+            if (!($detail = D('Goods')->find($goods_id))) {
+                $rs = array('success' => false, 'error_msg'=>'该商品不存在!');
+                die(json_encode($rs));
+            }
+            if ($detail['closed'] != 0 || $detail['audit'] != 1) {
+                $rs = array('success' => false, 'error_msg'=>'该商品不存在!');
+                die(json_encode($rs));
+            }
+            if ($detail['end_date'] < TODAY) {
+                $rs = array('success' => false, 'error_msg'=>'该商品已经过期，暂时不能购买!');
+                die(json_encode($rs));
+            }
+            if ($detail['num'] <= 0) {
+                $rs = array('success' => false, 'error_msg'=>'亲！没有库存了！');
+                die(json_encode($rs));
+            }
+            $cart = D('Usercart');
+            if($cart->checkCart($goods_id,$this->app_uid)){
+                $rs = array('success' => false, 'error_msg'=>'商品已添加');
+                die(json_encode($rs));
+            }else {
+                $data = array(
+                    'goods_id' => $goods_id,
+                    'user_id' => $this->app_uid,
+                    'create_time' => date('Y-m-d H:i:s', time())
+                );
+                $cart->add($data);
+                $rs = array('success' => true, 'error_msg' => '');
+                die(json_encode($rs));
+            }
+    }
+
+    public function cart_list(){
+        $cart = D('Usercart');
+        $cart_list = $cart->get_cart_list($this->app_uid);
+        $rs = array(
+            'success' => true,
+            'cart_list'=>$cart_list,
+            'error_msg' => ''
+        );
+        die(json_encode($rs));
+    }
+
+}
---
+++ Baocms/Lib/Model/UsercartModel.class.php
@@ -0,0 +1,26 @@
+<?php
+class UsercartModel extends CommonModel {
+
+    protected $pk = 'cart_id';
+    protected $tableName = 'user_cart';
+
+    
+    public function checkCart($goods_id,$app_uid) {
+        $goods_id = (int) $goods_id;
+        $app_uid = (int) $app_uid;
+        return $this->find(array('where' => array('goods_id' => $goods_id,'user_id' => $app_uid)));
+    }
+
+    public function get_cart_list($app_uid){
+        $Model = new Model();
+        $map = array('b.user_id' => $app_uid);
+        $items = $Model->table('bao_goods a')
+            ->field("a.*,b.num cart_num,b.create_time cart_cdate")
+            ->join('left join bao_users_cart b on a.goods_id = b.goods_id')
+            ->where($map)
+            ->order(array('b.cart_id' => 'desc'))
+            ->select();
+        return $items;
+    }
+
+}
