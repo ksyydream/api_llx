1aab9d00bcf73fd101cf50060c0ac7a88a479a01
zzz940zzz <zzz940zzz@hotmail.com>
2016-12-22 10:47:06 +0800

    testzzz

--- Baocms/Lib/Model/GoodsdianpingpicsModel.class.php
+++ Baocms/Lib/Model/GoodsdianpingpicsModel.class.php
@@ -6,17 +6,17 @@ class GoodsdianpingpicsModel extends CommonModel{
     protected $pk   = 'pic_id';
     protected $tableName =  'goods_dianping_pics';
 	
-	public function upload($order_id,$photos){
+	public function upload($order_id,$photos,$goods_id){
         $order_id = (int)$order_id;
         $this->delete(array("where"=>array('order_id'=>$order_id)));
         foreach($photos as $val){
-            $this->add(array('pic'=>$val,'order_id'=>$order_id));
+            $this->add(array('pic'=>$val,'order_id'=>$order_id,'goods_id'=>$goods_id));
         }
         return true;
     }
-    
-   
-    
+
+
+
     public function getPics($order_id){
         $order_id = (int)$order_id;
         return $this->where(array('order_id'=>$order_id))->select();
