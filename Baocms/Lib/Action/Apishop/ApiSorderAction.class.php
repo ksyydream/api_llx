e950b28421615269b634ac95e1fe1ba99ad5008c
ksyydream <249499109@qq.com>
2017-02-04 10:13:13 +0800

    llx

--- Baocms/Lib/Action/Apishop/ApiSorderAction.class.php
+++ Baocms/Lib/Action/Apishop/ApiSorderAction.class.php
@@ -146,6 +146,7 @@ class ApiSorderAction extends CommonAction{
         }
 
         $user_count = $Users->where(array('mobile' => $data['mobile']))->count();
+        die($this->ajaxReturn($this->getLastSql(),'JSON'));
         if ($user_count == 0) {
             $rs=array(
                 'success'=>false,
