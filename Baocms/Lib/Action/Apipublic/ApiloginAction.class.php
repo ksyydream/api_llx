65bd5db6c6d77b05c15e7a3b17574c2ac86f22fc
ksyydream <249499109@qq.com>
2017-02-11 12:06:54 +0800

    llx

--- Baocms/Lib/Action/Apipublic/ApiloginAction.class.php
+++ Baocms/Lib/Action/Apipublic/ApiloginAction.class.php
@@ -12,7 +12,7 @@ class ApiloginAction extends CommonAction{
         fwrite($open,var_export($dataall,true));
         fclose($open);
         $token= $this->get_token();
-        die('http://'.$this->_server('HTTP_HOST').'/Apipublic/WxPay/jsnotify');
+        die('http://'.$this->_server('HTTP_HOST').'/Apipublic/WxPay/appnotify');
         $rs = array(
             'success' => $token==-1?false:true,
             'user_id' => get_token_uid($token),
