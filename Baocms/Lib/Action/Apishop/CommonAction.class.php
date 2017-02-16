41f8481f70c1130ad230f0961963afa6ed027372
ksyydream <249499109@qq.com>
2017-02-16 16:48:26 +0800

    llx

--- Baocms/Lib/Action/Apishop/CommonAction.class.php
+++ Baocms/Lib/Action/Apishop/CommonAction.class.php
@@ -123,7 +123,7 @@ class CommonAction extends Action{
         import('ORG.Net.UploadFile');
         $data['face']='';
         $upload = new UploadFile(); //
-        $upload->maxSize = 102400; // 设置附件上传大小
+        $upload->maxSize = 1024000; // 设置附件上传大小
         $upload->allowExts = array('jpg', 'gif', 'png', 'jpeg'); // 设置附件上传类型
         $name = date('Y/m/d', NOW_TIME);
         $dir = BASE_PATH . '/attachs/' . $name . '/';
