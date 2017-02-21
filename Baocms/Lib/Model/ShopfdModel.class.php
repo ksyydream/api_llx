5a5a8e96b30a9235d33f10c36026c9b644134c57
ksyydream <249499109@qq.com>
2017-02-21 16:27:13 +0800

    llx

---
+++ Baocms/Lib/Model/ShopfdModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyang
+ * Date: 17/2/6
+ * Time: 下午1:29
+ */
+
+class ShopfdModel extends CommonModel{
+    protected $pk   = 'fd_id';
+    protected $tableName =  'shop_fd';
+
+    protected $id = 0;
+}
---
+++ Baocms/Lib/Model/ShopfdpicModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyang
+ * Date: 17/2/6
+ * Time: 下午1:29
+ */
+
+class ShopfdpicModel extends CommonModel{
+    protected $pk   = 'pic_id';
+    protected $tableName =  'shop_fd_pic';
+
+    protected $id = 0;
+}
