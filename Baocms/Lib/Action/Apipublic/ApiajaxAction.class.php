6c7c53f093aefcea104eeeca9871ec6960eea20a
ksyydream <249499109@qq.com>
2017-03-01 09:13:20 +0800

    llx

--- Baocms/Lib/Action/Apipublic/ApiajaxAction.class.php
+++ Baocms/Lib/Action/Apipublic/ApiajaxAction.class.php
@@ -73,4 +73,12 @@ class ApiajaxAction extends CommonAction{
         }
         echo $openid;
     }
+
+    public function down_Android(){
+        $info = D('Appinfo')->where("type=1")->order('version_num desc')->find();
+        if(file_exists(BASE_PATH.'/attachs/'.$info['path'])){
+            import('ORG.Net.Http');
+            Http::download(BASE_PATH.'/attachs/'.$info['path'],'拉拉秀安卓安装包');
+        }
+    }
 }
