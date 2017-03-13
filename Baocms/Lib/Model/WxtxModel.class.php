a322975738e62c250ef7f80c1ed38e1d46626da6
ksyydream <249499109@qq.com>
2017-03-13 09:55:09 +0800

    llx

---
+++ Baocms/Lib/Model/WxtxModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyang
+ * Date: 17/2/6
+ * Time: 下午1:29
+ */
+
+class WxtxModel extends CommonModel{
+    protected $pk   = 'id';
+    protected $tableName =  'wx_tx';
+
+    protected $id = 0;
+}
