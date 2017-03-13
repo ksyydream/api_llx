830b4b4f77c3f92cf5feb9c66374796f8025f94c
ksyydream <249499109@qq.com>
2017-03-13 11:49:22 +0800

    llx

--- Baocms/Lib/Action/Apiuser/WxtxAction.class.php
+++ Baocms/Lib/Action/Apiuser/WxtxAction.class.php
@@ -14,7 +14,7 @@ class WxtxAction extends CommonAction {
         //$list = $Pay->query("select a.*,shop_name from `bao_pay` AS a left join `bao_shop` AS b on a.shop_id = b.shop_id where a.mobile = ".$member['account']." order by a.id desc limit ".$page.",25");
         $list = D('Wxtx')->field('*')
             ->where('uid='.$this->app_uid)
-            ->order('a.id desc')
+            ->order('id desc')
             ->page($page . ',20')
             ->select();
         $rs=array('success'=>true,
