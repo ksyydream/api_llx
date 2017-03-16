3f2199a9a7aa6e3ab6ee7a28fab535686f2e6d84
ksyydream <249499109@qq.com>
2017-03-16 15:49:38 +0800

    llx

--- Baocms/Lib/Action/Apiuser/XiuAction.class.php
+++ Baocms/Lib/Action/Apiuser/XiuAction.class.php
@@ -89,7 +89,7 @@ class XiuAction extends CommonAction {
             ->select();
         foreach ($list as $k => $val) {
             $files=D('Xiuuserfile')->where(array('matser_id' => $val['id']))->select();
-            die(var_dump($files));
+            die(var_dump(D('Xiuuserfile')->getLastSql()));
             $list[$k]['files']=array();
             foreach ($files as $a => $v){
                 if(file_exists(BASE_PATH.'/attachs/'.$v['path'])){
