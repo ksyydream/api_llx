4e5c8ebe1248ecf1ae895bc7bcfe3c870895215c
ksyydream <249499109@qq.com>
2017-03-16 14:35:32 +0800

    llx

---
+++ Baocms/Lib/Action/Apiuser/XiuAction.class.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/12/20
+ * Time: 11:03
+ */
+class XiuAction extends CommonAction {
+
+    public function upload(){
+        $img = $this->upload2xiu('file');
+        if($img){
+            $rs = array(
+                'success' => true,
+                'error_msg' =>'',
+                'path'=>$img
+            );
+        }else{
+            $rs = array(
+                'success' => false,
+                'error_msg' =>'上传失败'
+            );
+        }
+        $this->ajaxReturn($rs,'JSON');
+    }
+
+    public function add_xiu(){
+        $remark = $this->_post('remark','trim,htmlspecialchars','');
+        if(!$remark){
+            $rs = array('success' => false, 'error_msg' =>'秀内容,不能为空!');
+            $this->ajaxReturn($rs,'JSON');
+        }
+
+        $photos = $this->_post('xiu_files');
+        if($photos){
+            if (!is_array($photos)){
+                $rs = array(
+                    'success' => false,
+                    'error_msg'=>'文件地址必须是数组!'
+                );
+                die(json_encode($rs));
+            }
+        }
+
+        $data = array(
+            'uid'=>$this->app_uid,
+            'remark'=>$remark,
+            'create_time'=>date('Y-m-d H:i:s'),
+            'hf_count'=>0,
+            'zan_count'=>0,
+            'liwu_count'=>0,
+            'flag'=>1
+        );
+
+        $master_id = D('Xiuuser')->add($data);
+
+        if($photos){
+            foreach($photos as $file){
+                if(file_exists(BASE_PATH.'/attachs/'.$file)){
+                    $path_data = array(
+                        'master_id'=>$master_id,
+                        'path'=>$file
+                    );
+                    $file_path = pathinfo(BASE_PATH.'/attachs/'.$file);
+                    $f_extension = isset($file_path['extension'])?$file_path['extension']:'';
+                    if($f_extension=='mp4' or $f_extension=='avi' or $f_extension=='3gp' or $f_extension=='wmv'){
+                        $path_data['flag']=2;
+                    }else{
+                        $path_data['flag']=1;
+                    }
+                   D('Xiuuserfile')->add($path_data);
+                }
+            }
+        }
+
+        $rs = array('success' => true, 'error_msg' =>'','xiuuser_id'=>$master_id);
+        $this->ajaxReturn($rs,'JSON');
+
+    }
+}
