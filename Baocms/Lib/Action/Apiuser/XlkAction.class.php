e3592e30234609fa7866e15522d81afc6ab3660a
ksyydream <249499109@qq.com>
2017-04-11 10:28:34 +0800

    llx

--- Baocms/Lib/Action/Apiuser/XlkAction.class.php
+++ Baocms/Lib/Action/Apiuser/XlkAction.class.php
@@ -7,45 +7,6 @@
  */
 class XlkAction extends CommonAction {
 
-    public function index() {
-        $user = D('Users')->find($this->app_uid);
-        $yhk = (array)json_decode($user['yhk']);
-        $yhk_n = array();
-        $zp = (array)json_decode($user['zp']);
-        $zp_n = array();
-        foreach($zp as $k=>$v){
-            $zp_n[$k] = (array)$v;
-        }
-        foreach($yhk as $k=>$v){
-            $shop = D('Shop')->find($k);
-            if($shop){
-                if(isset($zp_n[$k])){
-                    $yhk_n[] = array(
-                        'shop_id'=>$shop['shop_id'],
-                        'yhk'=>$v,
-                        'shop_name'=>$shop['shop_name'],
-                        'logo'=>$shop['logo'],
-                        'zp'=>$zp_n[$k]
-                    );
-                }else{
-                    $yhk_n[] = array(
-                        'shop_id'=>$shop['shop_id'],
-                        'yhk'=>$v,
-                        'shop_name'=>$shop['shop_name'],
-                        'logo'=>$shop['logo']
-                    );
-                }
-
-            }
-        }
-        $rs = array(
-            'success'=>true,
-            'yhk'=>$yhk_n,
-            'error_msg'=>''
-        );
-        $this->ajaxReturn($rs,'JSON');
-    }
-
     public function use_xlk(){
         $xlh = $this->_post('xlh');
         if(!$xlh){
@@ -144,5 +105,16 @@ class XlkAction extends CommonAction {
         //D('Xlktimes')->delete_times($this->app_uid);
         $rs = array('success'=>true, 'error_msg'=>'');
         $this->ajaxReturn($rs,'JSON');
+    }
+
+    public function used_list(){
+        $page = $this->_param('page', 'htmlspecialchars')?$this->_param('page', 'htmlspecialchars'):1;
+        $list = D('Xlkdetail')->get_list($this->app_uid,$page);
+        $rs = array(
+            'success'=>true,
+            'error_msg'=>'',
+            'list'=>$list
+        );
+        die(json_encode($rs));
     }
 }
--- Baocms/Lib/Model/XlkdetailModel.class.php
+++ Baocms/Lib/Model/XlkdetailModel.class.php
@@ -11,4 +11,13 @@ class XlkdetailModel extends CommonModel{
     protected $tableName =  'xlk_detail';
 
     protected $id = 0;
+
+    public function get_list($uid,$page=1){
+        $list = $this->alias('a')->field('a.*,b.title,c.shop_name')
+            ->join('bao_xlk_master b on a.master_id = b.id','LEFT')
+            ->join('bao_shop c on b.shop_id = c.shop_id','LEFT')
+            ->where(array('a.flag'=>2,'a.uid'=>$uid))
+            ->order(array('a.used_time' => 'asc'))->page($page.',10')->select();
+        return $list;
+    }
 }
